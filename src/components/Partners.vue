d962be822b0c9945b599dc2c910ae8a7808305c3
sergiojanko <janko.sergio1998@gmail.com>
2021-09-17 15:43:11 +0200

    add partners

--- src/components/Partners.vue
+++ src/components/Partners.vue
@@ -1,20 +1,20 @@
 <template>
   <section id="partners">
-    <div class="container">
+    <div class="container text-center">
       <div class="row">
         <div class="col-12">
           <h2>What Our Clients Say</h2>
         </div>
         <div class="col-12">
-          <div class="partners">
-            <img src="@/assets/images/testimonials-1.jpg" alt="">
-            <p class="partner-text"></p>
-            <p class="partner-name"></p>
+          <div v-for="(partner, i) in partners" :key="i" class="partners" :class="partner.active ? 'active':'d-none'">
+            <img :src="getImageUrl(partner.img)" alt="">
+            <p class="partner-text">{{partner.text}}</p>
+            <p class="partner-name">{{partner.name}}</p>
           </div>
         </div>
         <hr>
-        <div class="col-3">
-          <div class="partner-logo"></div>
+        <div v-for="logo in logos" :key="logo" class="col-3">
+          <div class="partner-logo"><img :src="getImageUrl(logo)" alt=""></div>
         </div>
       </div>
       
@@ -25,9 +25,36 @@
 <script>
 export default {
 name:"Partners",
+data(){
+  return{
+    partners: [
+      {
+        img: "testimonials-2.jpg",
+        text: "Ability proceeds from a fusion of skills, knowledge, understanding and imagination",
+        name: "Luis Desalvo",
+        active: true,
+      },
+      {
+        img: "testimonials-1.jpg",
+        text: "Ability proceeds from a fusion of skills, knowledge, understanding and imagination",
+        name: "Luisa Salvode",
+        active: false,
+      },
+    ],
+    logos: ["clients_partner_1-200x202.png","clients_partner_2-200x202.png","clients_partner_3-200x202.png","clients_partner_4-200x202.png"],
+  }
+},
+methods: {
+  getImageUrl(img){
+    return require("../assets/images/" + img);
+  },
+}
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.partners img 
+{
+  border-radius: 50%;
+}
 </style>
